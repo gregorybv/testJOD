0677334393591e1d4ef46a63d714a65567fd30dc
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-24 10:40:37 +0300

    new image

--- src/products.js
+++ src/products.js
@@ -1,50 +1,72 @@
 export const products = [
   {
-    imageUrl: 'https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Big Mack',
-    brand: 'Burgers',
-    badge: 'New',
+    imageUrl:
+      "https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Hamburger",
+    brand: "Burgers",
+    badge: "Top!!!",
   },
   {
-    imageUrl: 'https://images.unsplash.com/photo-1550317138-10000687a72b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Mack',
-    brand: 'Burgers',
-    badge: 'Top',
+    imageUrl:
+      "https://images.unsplash.com/photo-1610970878459-a0e464d7592b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Chisburger",
+    brand: "Burgers",
+    badge: "New!!!",
   },
   {
-    imageUrl: 'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Hamburger',
-    brand: 'Burgers',
-    badge: 'Top!!!',
+    imageUrl:
+      "https://images.unsplash.com/photo-1576843776838-032ac46fbb93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Hamburger",
+    brand: "Burgers",
+    badge: "Top!!!",
   },
   {
-    imageUrl: 'https://images.unsplash.com/photo-1610970878459-a0e464d7592b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Chisburger',
-    brand: 'Burgers',
-    badge: 'New!!!',
+    imageUrl:
+      "https://images.unsplash.com/photo-1606755962773-d324e0a13086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Chisburger",
+    brand: "Burgers",
+    badge: "New!!!",
   },
   {
-    imageUrl: 'https://images.unsplash.com/photo-1576843776838-032ac46fbb93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Hamburger',
-    brand: 'Burgers',
-    badge: 'Top!!!',
+    imageUrl:
+      "https://images.unsplash.com/photo-1627492375578-e3f049c43633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTd8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Hamburger",
+    brand: "Burgers",
+    badge: "Top",
   },
   {
-    imageUrl: 'https://images.unsplash.com/photo-1606755962773-d324e0a13086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Chisburger',
-    brand: 'Burgers',
-    badge: 'New!!!',
+    imageUrl:
+      "https://images.unsplash.com/photo-1544037803-ed377ec9a75e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjN8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Chisburger",
+    brand: "Burgers",
+    badge: "New",
   },
   {
-    imageUrl: 'https://images.unsplash.com/photo-1627492375578-e3f049c43633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTd8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Hamburger',
-    brand: 'Burgers',
-    badge: 'Top!!!',
+    imageUrl:
+      "https://images.unsplash.com/photo-1606755962773-d324e0a13086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Chisburger",
+    brand: "Burgers",
+    badge: "New!!!",
   },
   {
-    imageUrl: 'https://images.unsplash.com/photo-1544037803-ed377ec9a75e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjN8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Chisburger',
-    brand: 'Burgers',
-    badge: 'New!!!',
+    imageUrl:
+      "https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Hamburger",
+    brand: "Burgers",
+    badge: "Top!!!",
   },
-];
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1477617722074-45613a51bf6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzh8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Hamburger",
+    brand: "Burgers",
+    badge: "Top",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1627492375625-283ba02dd2eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODB8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    name: "Chisburger",
+    brand: "Burgers",
+    badge: "New",
+  },
+]
