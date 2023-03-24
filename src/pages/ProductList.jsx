2bd3a7604129b9bd98324e786a3bfa0c05f823ed
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-24 10:23:58 +0300

    new edits

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,33 +1,9 @@
 import React, { useState } from 'react';
-import ListItem from '../components/ListItem';
+import ListItem from '../components/ListItem/ListItem';
 import Pagination from '../components/Pagination/Pagination';
+import { products } from '@/products';
 
-const products = [
-  {
-    imageUrl: 'https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Burger 1',
-    brand: 'Big Mack',
-    badge: 'New',
-  },
-  {
-    imageUrl: 'https://images.unsplash.com/photo-1550317138-10000687a72b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Burger 2',
-    brand: 'Mack',
-    badge: 'Top',
-  },
-  {
-    imageUrl: 'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Burger 3',
-    brand: 'Hamburger',
-    badge: 'Top!!!',
-  },
-  {
-    imageUrl: 'https://images.unsplash.com/photo-1610970878459-a0e464d7592b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    name: 'Burger 4',
-    brand: 'Chisburger',
-    badge: 'New!!!',
-  },
-];
+
 
 const PAGE_SIZE = 2;
 
@@ -46,7 +22,7 @@ const ProductList = () => {
 
   return (
     <div>
-      <div style={{display:"flex", gap: '160px'}}>
+      <div style={{display:"flex", gap: '160px', flexWrap: 'wrap', margin: '0 auto'}}>
         {productsToShow.map((product, index) => (
           <ListItem
             key={index}
